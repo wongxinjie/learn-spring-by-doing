dda589d9e3c70ecc4ee12d350310dbbb6651f130
wongxinjie <wongxinjierun@gmail.com>
2018-03-16 14:42:09 +0800

    modify controller return

--- hackernews/src/main/java/com/wongxinjie/hackernews/controller/TopicController.java
+++ hackernews/src/main/java/com/wongxinjie/hackernews/controller/TopicController.java
@@ -1,6 +1,6 @@
 package com.wongxinjie.hackernews.controller;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.sun.corba.se.impl.oa.toa.TOA;
 import com.wongxinjie.hackernews.bean.PageResultBean;
 import com.wongxinjie.hackernews.bean.ResultBean;
 import com.wongxinjie.hackernews.entity.Topic;
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,55 +29,58 @@ public class TopicController {
     private TopicService topicService;
 
     @GetMapping(value = "topics", produces = "application/json")
-    public PageResultBean<Topic> getAllTopic() {
+    public ResponseEntity<PageResultBean<Topic>> getAllTopic() {
         List<Topic> topics = topicService.getAllTopic();
         PageResultBean<Topic> page = new PageResultBean<>(topics, 1, topics.size(), topics.size(), 1);
-        return page;
+        return ResponseEntity.ok(page);
     }
 
     @GetMapping(value = "v1.0/topics", params = {"page", "size"}, produces = "application/json")
-    public PageResultBean<Topic> getTopicByPage(
+    public ResponseEntity<PageResultBean<Topic>> getTopicByPage(
             @RequestParam(value = "page", required = false, defaultValue = "1") int page,
             @RequestParam(value = "size", required = false, defaultValue = "20") int pageSize) {
 
         Page<Topic> topicPaginator = topicService.getTopicByPage(page, pageSize);
-        return new PageResultBean<>(topicPaginator);
+        PageResultBean<Topic> pages = new PageResultBean<>(topicPaginator);
+        return ResponseEntity.ok(pages);
     }
 
     @GetMapping(value = "v1.0/topics/{id}", produces = "application/json")
-    public ResultBean<Topic> getTopicById(@PathVariable("id") Integer id) {
+    public ResponseEntity<ResultBean<Topic>> getTopicById(@PathVariable("id") Integer id) {
         Topic topic = topicService.getTopicById(id);
-        return new ResultBean<>(topic);
+        return ResponseEntity.ok(new ResultBean<>(topic));
     }
 
     @PostMapping(value = "v1.0/topics", produces = "application/json")
-    public ResultBean<Long> createTopic(@RequestBody Topic topic) {
+    public ResponseEntity<ResultBean<Long>> createTopic(@RequestBody Topic topic) {
         long topicId = topicService.addTopic(topic);
 
-        ResultBean response = new ResultBean<>(topicId);
+        ResultBean<Long> response = new ResultBean<>(topicId);
         if(topicId == 0) {
             response.setCode(419);
             response.setMessage("topic already exists");
+            return new ResponseEntity<>(response, HttpStatus.CONFLICT);
         }
-        return response;
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping(value = "v1.0/topics/{id}", produces = "application/json")
-    public ResultBean<Long> updateTopic(@PathVariable("id") Integer id, @RequestBody Topic topic) {
+    public ResponseEntity<ResultBean<Long>> updateTopic(@PathVariable("id") Integer id, @RequestBody Topic topic) {
         long topicId = topicService.updateTopic(id, topic);
 
-        ResultBean response = new ResultBean<>(topicId);
+        ResultBean<Long> response = new ResultBean<>(topicId);
         if(topicId == 0) {
             response.setCode(404);
             response.setMessage("topic not exists");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
         }
-        return response;
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping(value="v1.0/topics/{id}", produces = "application/json")
-    public ResultBean<Boolean> deleteTopic(@PathVariable("id") Integer id) {
+    public ResponseEntity<ResultBean<Boolean>> deleteTopic(@PathVariable("id") Integer id) {
         boolean success = topicService.deleteTopic(id);
-        return new ResultBean<>(success);
+        return ResponseEntity.ok(new ResultBean<>(success));
     }
 
 }
